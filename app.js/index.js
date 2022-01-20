18e38ea2f2caf4cd7752bba53af9f1380253769a
la-viza <97736137+la-viza@users.noreply.github.com>
2022-01-20 13:37:27 +0300

    Added working unit conversion links

--- app.js/index.js
+++ app.js/index.js
@@ -38,6 +38,8 @@ function showWeather(response) {
     let iconElement = document.querySelector ("#main-icon");
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
+    
+    celsiusTemperature = response.data.main.temp;
 }
 
 function searchCity(city) {
@@ -65,7 +67,33 @@ function showLocation(position) {
 function getCurrentPosition() {
   navigator.geolocation.getCurrentPosition(showLocation);
 }
+
+function displayFahrenheitTemperature(event) {
+event.preventDefault();
+let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+let temperatureElement = document.querySelector ("#temperature");
+celsiusLink.classList.remove("active");
+fahrenheitLink.classList.add("active");
+temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector ("#temperature");
+  temperatureElement.innerHTML=Math.round(celsiusTemperature);  
+}
+
+let celsiusTemperature=null;
+
 let button = document.querySelector("#current-location-btn");
 button.addEventListener("click", getCurrentPosition);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("Toronto");
